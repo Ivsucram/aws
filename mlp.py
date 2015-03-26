82a9bd6e7d5dbd3a989698629105ddbd6b31bced
Ivsucram <ivsucram@gmail.com>
2015-03-27 04:05:45 +0800

    distortion on the mlp

--- mlp.py
+++ mlp.py
@@ -180,6 +180,26 @@ def write_on_file_and_console(text, results_file):
     results_file.write('\n')
     results_file.flush()
 
+def elastic_distortion(input, x):
+        image = Image.fromarray(np.array(np.split(np.array(input)*255,28)))
+        image = image.rotate(np.random.randint(-30,30))
+        x = (x % 10)*2+10
+        image = image.transform((x,x), Image.EXTENT, [np.random.randint(0, 4), np.random.randint(0, 4), 28, 28])
+        image = image.resize((28,28))
+        return np.array(list(image.getdata()))/255
+    
+def create_distortion_dataset():
+    dataset = 'data/mnist_batches.npz'
+    epoch = len(os.walk('distort').next()[2]) + 1
+    
+    mnist = np.load(dataset)
+    train_set_x = mnist['train_data']
+    mnist = None
+    for i in xrange(len(train_set_x)):
+        train_set_x[i] = elastic_distortion(input=train_set_x[i], x=np.random.choice(9, 1, p=[0.03, 0.03, 0.04, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2])[0])
+    
+    return train_set_x
+
 def test_mlp(
         initial_learning_rate,
         learning_rate_decay,
@@ -314,7 +334,7 @@ def test_mlp(
         n_epochs = original_epochs
         
         while epoch_counter < n_epochs and this_validation_errors > 0:
-            train_set_x_with_distortion = theano.shared(np.asarray(np.load("distort/"+ str(np.random.randint(1,len(os.walk('distort').next()[2]))) +".npy"), dtype=theano.config.floatX))
+            train_set_x_with_distortion = theano.shared(np.asarray(create_distortion_dataset, dtype=theano.config.floatX))
             train_model_distortion = theano.function(inputs=[epoch, index], outputs=[classifier.errors(y), output],
                     updates=updates,
                     givens={
@@ -810,7 +830,7 @@ def test_dark_knowledge(
         n_epochs = original_epochs
         
         while epoch_counter < n_epochs and this_validation_errors > 0:
-            train_set_x_with_distortion = theano.shared(np.asarray(np.load("distort/"+ str(np.random.randint(1,len(os.walk('distort').next()[2]))) +".npy"), dtype=theano.config.floatX))                
+            train_set_x_with_distortion = theano.shared(np.asarray(create_distortion_dataset, dtype=theano.config.floatX))            
             train_model_distortion = theano.function(inputs=[epoch, index], outputs=[classifier.errors(y), output],
                 updates=updates,
                 givens={
@@ -903,7 +923,7 @@ if __name__ == '__main__':
         
         learning_rate_decay = 0.999
         squared_filter_length_limit = 15.0
-        n_epochs = 200
+        n_epochs = 300
         n_matches = 1
         patient = n_epochs*0.75
         batch_size = 100
